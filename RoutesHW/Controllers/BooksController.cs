26dbd95b2e5f0e826ab1fc31552b23f1770be74b
rotemdan267 <rotemdan267@gmail.com>
2022-05-27 00:01:45 +0300

    Add project files.

---
+++ RoutesHW/Controllers/BooksController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RoutesHW.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [Route("{id:int:min(6)}")]
+        public string GetByIdINT(int id)
+        {
+            return "book INT > 5 " + id;
+        }
+
+
+        [Route("{id:regex(book(z|b)oom)}")]
+        public string GetByIdREG(string id)
+        {
+            return "book REGEX " + id;
+        }
+
+        //[Route("{id:alpha}")]
+        //public string GetByIdAlpha(string id)
+        //{
+        //    return "book Letters-Only " + id;
+        //}
+    }
+}
